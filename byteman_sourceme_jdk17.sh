2d48c4d28c54fcb292915b3592ead1e331e27113
Dmitry Zayats <dmitryzayats@yahoo.com>
2025-10-30 11:45:56 -0500

    Added sourceme file

---
+++ byteman_sourceme_jdk17.sh
@@ -0,0 +1,3 @@
+export BYTEMAN_HOME=~/fun_with_byteman/byteman-download-4.0.25
+export JAVA_HOME=~/fun_with_byteman/openlogic-openjdk-17.0.14+7-linux-x64
+export PATH=$PATH:$BYTEMAN_HOME/bin:$JAVA_HOME/bin
